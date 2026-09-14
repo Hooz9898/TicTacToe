bb74cb823d095c55fd413c96f34ab95ffdabea46
Hooz9898 <128hsarwar@frhsd.com>
2026-09-14 10:19:18 -0400

    Commit Foundation

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner sc = new Scanner(System.in);
+        String[][] tictactoe = new String[3][3];
+        String x1;
+        String o1;
+        Boolean cross = false;
+        System.out.println("");
+
     }
 }
